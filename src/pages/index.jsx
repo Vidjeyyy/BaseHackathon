5e4a12f1637d1edaa0cf379d1b10f157929bf321
Reillat Anthony <anthony.reillat@epita.fr>
2023-05-11 13:04:38 +0200

    init project

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,3 @@
 export const Homepage = () => {
-    return (
-        <div> Salut </div>
-    )
-}
+  return <div> Salut</div>;
+};
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,3 @@
 export const Login = () => {
-    return (
-        <div> Salut </div>
-    )
-}
+  return <div> Salut </div>;
+};
